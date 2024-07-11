b66e7d2496392256e1f28c16a86d412347b91243
izzy2k2 <isabellelapolla@gmail.com>
2024-07-10 18:49:57 -0700

    Check for win functionality

--- GameTracker.java
+++ GameTracker.java
@@ -12,7 +12,34 @@ public class GameTracker{
         boolean gameWin = false;
         if(gameBoard[subGame].claimBox(box, player)){
             // check for full game win
+            if(checkWin(subGame)){
+                // game is won
+            }
         }
         return gameWin;
+    }
+
+    private boolean checkWin(int subToCheck){
+        int rowNo = subToCheck % 3;
+        int colNo = subToCheck - (rowNo * 3);
+        boolean isWin = false;
+        if(gameBoard[subToCheck].getStatus() != '0'){
+
+            // row check
+            isWin = gameBoard[rowNo].getVal() == gameBoard[rowNo + 1].getVal() && gameBoard[rowNo].getVal() == gameBoard[rowNo + 2].getVal();
+
+            // column check
+            isWin = isWin || (gameBoard[colNo].getVal() == gameBoard[colNo + 3].getVal() && gameBoard[colNo].getVal() == gameBoard[colNo + 6].getVal());
+
+            if(!isWin && subToCheck % 2 == 0){
+                if(subToCheck % 4 == 0){
+                    isWin = gameBoard[0].getVal() == gameBoard[4].getVal() && gameBoard[0].getVal() == gameBoard[8].getVal();
+                }
+                else{
+                    isWin = gameBoard[2].getVal() == gameBoard[4].getVal() && gameBoard[2].getVal() == gameBoard[6].getVal();
+                }
+            }
+        }
+        return isWin;
     }
 }

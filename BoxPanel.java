0f0f88841a509aa3d1a41dd4035b2515d834ae07
izzy2k2 <isabellelapolla@gmail.com>
2024-07-12 21:55:09 -0700

    Making things

---
+++ BoxPanel.java
@@ -0,0 +1,10 @@
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class BoxPanel extends JPanel{
+    public BoxPanel(){
+        this.setBackground(new Color(0xffebcd));
+        // keep going, need boundaries
+    }
+}
---
+++ PanelMaker.java
@@ -0,0 +1,8 @@
+import javax.swing.JPanel;
+
+public class PanelMaker extends JPanel{
+    public PanelMaker(){
+        JPanel topLeft = new JPanel();
+        JPanel topMid = new JPanel();
+    }
+}
